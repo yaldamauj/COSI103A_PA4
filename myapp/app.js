d9c34c566373bb0e33b29d555f644d2c001a8a17
Yalda Mauj <64442722+yaldamauj@users.noreply.github.com>
2023-04-30 19:04:22 -0400

    base code

--- myapp/app.js
+++ myapp/app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +19,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+//new added code
+app.use((req,res,next) =>{
+  console.log('we are handling the request')
+  req.mynote='demo'
+  next()
+})
+
+app.get('/', (req, res, next)=> {
+  res.render('index');
+})
+
+app.get('/info', (req,res,next) => {
+  res.locals.time = Date()
+  res.locals.owner = "Yalda Mauj"
+  res.locals.age = 22
+  res.render('info')
+})
+
+app.post('/getData',(req,res,next) => {
+  console.dir(req.body)
+  res.json(req.body)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
